89467e13741df41efc7fa70ab180390b9d795051
rhodrin <rhodri.nelson@gmail.com>
2020-08-21 13:59:45 +0100

    test_roundoff: Test for various r.

--- tests/test_roundoff.py
+++ tests/test_roundoff.py
@@ -8,21 +8,21 @@ class TestRoundoff(object):
     """
     Class for testing SubDomains
     """
-
-    def test_lm_forward(self):
+    @pytest.mark.parametrize('dat', [0.5, 0.624, 1.0, 1.5, 2.0, 3.0, 3.6767, 4.0], 'dtype', [np.float32, np.float64])
+    def test_lm_forward(self, dat, dtype):
         """
         ...
         """
         iterations = 10000
         r= Constant(name='r')
-        r.data = 4.0
+        r.data = dat
         s = 0.01
 
         grid = Grid(shape=(2, 2), extent=(1, 1))
         dt = grid.stepping_dim.spacing
 
-        f0 = TimeFunction(name='f0', grid=grid, time_order=2)
-        f1 = TimeFunction(name='f1', grid=grid, time_order=2, save=iterations+2)
+        f0 = TimeFunction(name='f0', grid=grid, time_order=2, dtype=dtype)
+        f1 = TimeFunction(name='f1', grid=grid, time_order=2, save=iterations+2, dtype=dtype)
 
         lmap0 = Eq(f0.forward, r*f0*(1.0-f0+(1.0/s)*dt*f0.forward-f0.forward))
         lmap1 = Eq(f1.forward, r*f1*(1.0-f1+(1.0/s)*dt*f1.forward-f1.forward))
@@ -40,13 +40,14 @@ class TestRoundoff(object):
 
         assert np.allclose(f0.data[np.mod(iterations+1,3)], f1.data[iterations+1], atol=0, rtol=0)
 
-    def test_lm_backward(self):
+    @pytest.mark.parametrize('dat', [0.5, 0.624, 1.0, 1.5, 2.0, 3.0, 3.6767, 4.0])
+    def test_lm_backward(self, dat):
         """
         ...
         """
         iterations = 10000
         r= Constant(name='r')
-        r.data = 4.0
+        r.data = dat
         s = 0.01
 
         grid = Grid(shape=(2, 2), extent=(1, 1))
@@ -71,13 +72,14 @@ class TestRoundoff(object):
 
         assert np.allclose(f0.data[np.mod(iterations+1,3)], f1.data[iterations+1], atol=0, rtol=0)
 
-    def test_lm_fb(self):
+    @pytest.mark.parametrize('dat', [0.5, 0.624, 1.0, 1.5, 2.0, 3.0, 3.6767, 4.0])
+    def test_lm_fb(self, dat):
         """
         ...
         """
         iterations = 10000
         r= Constant(name='r')
-        r.data = 4.0
+        r.data = dat
         s = 0.01
 
         grid = Grid(shape=(2, 2), extent=(1, 1))
